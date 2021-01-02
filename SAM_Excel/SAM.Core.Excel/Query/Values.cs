c7c6028b5297dd016c03167a52cd6c6a20309754
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-02 21:44:16 +0000

    Values Query Added

--- SAM_Excel/SAM.Core.Excel/Query/Values.cs
+++ SAM_Excel/SAM.Core.Excel/Query/Values.cs
@@ -1,5 +1,6 @@
 ﻿using NetOffice.ExcelApi;
 using System;
+using System.Collections.Generic;
 
 namespace SAM.Core.Excel
 {
@@ -24,7 +25,7 @@ namespace SAM.Core.Excel
                 Workbook workbook = application.Workbooks.Open(path);
                 Worksheet worksheet = workbook.Worksheet(worksheetName);
                 if (worksheet != null)
-                    result = (object[,])worksheet.UsedRange.Value;
+                    result = worksheet.UsedRange?.Value as object[,];
             }
             catch(Exception exception)
             {
@@ -33,6 +34,50 @@ namespace SAM.Core.Excel
             finally
             {
                 if(application != null)
+                {
+                    application.Quit();
+                    application.Dispose();
+                }
+            }
+
+            return result;
+        }
+
+        public static Dictionary<string, object[,]> Values(string path)
+        {
+            if (string.IsNullOrWhiteSpace(path))
+                return null;
+
+            if (!System.IO.File.Exists(path))
+                return null;
+
+            Application application = null;
+            Dictionary<string, object[,]> result = null;
+
+            try
+            {
+                application = new Application();
+                application.DisplayAlerts = false;
+
+                Workbook workbook = application.Workbooks.Open(path);
+
+                int count = workbook.Worksheets.Count;
+
+                result = new Dictionary<string, object[,]>();
+                for (int i = 0; i < count; i++)
+                {
+                    Worksheet worksheet = workbook.Worksheets[i + 1] as Worksheet;
+                    if (worksheet != null)
+                        result[worksheet.Name] = worksheet?.UsedRange?.Value as object[,];
+                }
+            }
+            catch (Exception exception)
+            {
+
+            }
+            finally
+            {
+                if (application != null)
                 {
                     application.Quit();
                     application.Dispose();
---
+++ SAM_Excel/SAM.Core.Excel/Query/WorksheetNames.cs
@@ -0,0 +1,26 @@
+﻿using NetOffice.ExcelApi;
+using System.Collections.Generic;
+
+namespace SAM.Core.Excel
+{
+    public static partial class Query
+    {
+        public static List<string> WorksheetNames(this Workbook workbook)
+        {
+            if (workbook == null || workbook.Worksheets == null)
+                return null;
+
+            List<string> result = new List<string>();
+
+            int count = workbook.Worksheets.Count;
+
+            for(int i=0; i < count; i++)
+            {
+                Worksheet worksheet = workbook.Worksheets[i + 1] as Worksheet;
+                result.Add(worksheet?.Name);
+            }
+
+            return result;
+        }
+    }
+}
