673b1a59aa69f6fe2ee20dea63141497c2a90671
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-01-04 16:33:31 +0000

    Write Excel Initail Implementation (WIP)

---
+++ Grasshopper/SAM.Core.Grasshopper.Excel/Component/SAMExcelWrite.cs
@@ -0,0 +1,117 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Parameters;
+using SAM.Core.Grasshopper.Excel.Properties;
+using System;
+using System.Collections.Generic;
+
+namespace SAM.Core.Grasshopper.Excel
+{
+    public class SAMExcelWrite : GH_SAMVariableOutputParameterComponent
+    {
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid => new Guid("c51ea883-e931-41c0-8e44-0be46627c51a");
+
+        /// <summary>
+        /// The latest version of this component
+        /// </summary>
+        public override string LatestComponentVersion => "1.0.0";
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon => Resources.SAM_Small;
+
+        public override GH_Exposure Exposure => GH_Exposure.tertiary | GH_Exposure.obscure;
+
+        /// <summary>
+        /// Initializes a new instance of the SAM_point3D class.
+        /// </summary>
+        public SAMExcelWrite()
+          : base("SAMExcel.Write", "SAMExcel.Write",
+              "Write Microsoft Excel File",
+              "SAM", "Excel")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Inputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+                result.Add(new GH_SAMParam(new Param_String() { Name = "_path", NickName = "_path", Description = "Path to Microsoft Excel File", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new Param_String() { Name = "_worksheetName", NickName = "_worksheetName", Description = "Worksheet Name", Access = GH_ParamAccess.item }, ParamVisibility.Binding));
+                result.Add(new GH_SAMParam(new Param_GenericObject() { Name = "_values", NickName = "_values", Description = "Values", Access = GH_ParamAccess.tree }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override GH_SAMParam[] Outputs
+        {
+            get
+            {
+                List<GH_SAMParam> result = new List<GH_SAMParam>();
+
+                result.Add(new GH_SAMParam(new Param_Boolean() { Name = "Successful", NickName = "Successful", Description = "Successful", Access = GH_ParamAccess.tree }, ParamVisibility.Binding));
+                return result.ToArray();
+            }
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="dataAccess">
+        /// The DA object is used to retrieve from inputs and store in outputs.
+        /// </param>
+        protected override void SolveInstance(IGH_DataAccess dataAccess)
+        {
+            string path = null;
+            if (!dataAccess.GetData(0, ref path) || string.IsNullOrEmpty(path))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            if (!System.IO.File.Exists(path))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "File does not exists");
+                return;
+            }
+
+            string worksheetName = null;
+            if (!dataAccess.GetData(1, ref worksheetName) || string.IsNullOrEmpty(worksheetName))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            global::Grasshopper.Kernel.Data.GH_Structure<global::Grasshopper.Kernel.Types.IGH_Goo> structure;
+            if (!dataAccess.GetDataTree(2, out structure))
+            {
+                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Invalid data");
+                return;
+            }
+
+            object[,] values = null; //= Query.Objects<object>(structure);
+
+            if (values == null)
+                return;
+
+            int index = Params.IndexOfOutputParam("Values");
+            if (index == -1)
+                return;
+
+            DataTree<object> dataTree = Query.DataTree(values);
+
+            dataAccess.SetDataTree(index, dataTree);
+        }
+    }
+}
---
+++ SAM_Excel/SAM.Core.Excel/Modify/Write.cs
@@ -0,0 +1,91 @@
+﻿using NetOffice.ExcelApi;
+using System;
+
+namespace SAM.Core.Excel
+{
+    public static partial class Modify
+    {
+        public static bool Write(this Worksheet worksheet, object[,] values)
+        {
+            if (worksheet == null)
+                return false;
+
+            if (values == null)
+            {
+                worksheet.Cells.Clear();
+                return true;
+            }
+
+            int lowerBound_1 = values.GetLowerBound(0);
+            if (lowerBound_1 == 0)
+                lowerBound_1 += 1;
+
+            int lowerBound_2 = values.GetLowerBound(1);
+            if (lowerBound_2 == 0)
+                lowerBound_2 += 1;
+
+            Range range_1 = worksheet.Cells[lowerBound_1, lowerBound_2];
+            Range range_2 = worksheet.Cells[values.GetUpperBound(0) + lowerBound_1, values.GetUpperBound(1) + lowerBound_2];
+
+            Range range = worksheet.Range(range_1, range_2);
+
+            range.Value = values;
+            return true;
+        }
+
+        public static bool Write(this Workbook workbook, string worksheetName, object[,] values)
+        {
+            if (workbook == null || string.IsNullOrEmpty(worksheetName))
+                return false;
+
+            Worksheet worksheet = workbook.Worksheet(worksheetName);
+            if (worksheet == null)
+            {
+                worksheet = workbook.Worksheets.Add() as Worksheet;
+                if (worksheet != null)
+                    worksheet.Name = worksheetName;
+            }
+
+            return Write(worksheet, values);
+        }
+
+        public static bool Write(string path, string worksheetName, object[,] values)
+        {
+            if (string.IsNullOrEmpty(path) || string.IsNullOrEmpty(worksheetName))
+                return false;
+
+            Application application = null;
+            bool result = false;
+
+            try
+            {
+                application = new Application(true);
+                application.DisplayAlerts = false;
+
+                Workbook workbook = null;
+                if(System.IO.File.Exists(path))
+                    workbook = application.Workbooks.Open(path);
+                else
+                    workbook = application.Workbooks.Add();
+
+                result = Write(workbook, worksheetName, values);
+                if(result)
+                    workbook.SaveAs(path);
+            }
+            catch (Exception exception)
+            {
+                result = false;
+            }
+            finally
+            {
+                if (application != null)
+                {
+                    application.Quit();
+                    application.Dispose();
+                }
+            }
+
+            return result;
+        }
+    }
+}
