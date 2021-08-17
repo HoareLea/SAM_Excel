795d434f1d273847d8b2774fe7f1fcba6e73f181
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2021-08-17 10:49:27 +0100

    Convert Excel To Pdf Method Added

---
+++ SAM_Excel/SAM.Core.Excel/Convert/ToPdf.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using NetOffice.ExcelApi;
+
+namespace SAM.Core.Excel
+{
+    public static partial class Convert
+    {
+        public static string ToPdf(string excelPath)
+        {
+            if (string.IsNullOrWhiteSpace(excelPath) || !File.Exists(excelPath))
+            {
+                return null;
+            }
+
+            string pdfPath = Path.Combine(Path.GetDirectoryName(excelPath), Path.GetFileNameWithoutExtension(excelPath) + ".pdf");
+            return ToPdf(excelPath, pdfPath);
+        }
+
+        public static string ToPdf(string excelPath, string pdfPath)
+        {
+            if (string.IsNullOrWhiteSpace(excelPath) || !File.Exists(excelPath) || string.IsNullOrWhiteSpace(pdfPath))
+            {
+                return null;
+            }
+
+            if(!Directory.Exists(Path.GetDirectoryName(pdfPath)))
+            {
+                return null;
+            }
+
+            Application application = new Application();
+            application.DisplayAlerts = false;
+
+            Workbook workbook = application.Workbooks.Open(excelPath);
+
+            workbook.ExportAsFixedFormat(NetOffice.ExcelApi.Enums.XlFixedFormatType.xlTypePDF, pdfPath);
+
+            application.Quit();
+            application.Dispose();
+
+            return pdfPath;
+        }
+    }
+}
+
